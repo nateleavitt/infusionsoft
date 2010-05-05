9267e807703d4ab7c3ad496a5c786c4f7876a3cd
Nathan Leavitt <nate@infusedsystems.com>
2010-05-05 13:20:36 -0700

    bug fix

--- test/api_infusion_test.rb
+++ test/api_infusion_test.rb
@@ -3,6 +3,6 @@ require 'test/unit'
 class ApiInfusionTest < Test::Unit::TestCase
   # Replace this with your real tests.
   def test_this_plugin
-    flunk
+    flunk # will implement later :(
   end
 end
