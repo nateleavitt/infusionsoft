fcc8b58d6b5ef44dee6490cd66f2ca8def293ab0
Nathan <nate@infusedsystems.com>
2011-01-24 16:08:27 -0700

    added some nil checks for Infusion transient connection errors

--- lib/api_infusionsoft.rb
+++ lib/api_infusionsoft.rb
@@ -1,33 +1,41 @@
 require "xmlrpc/client"
 
 class InfusionsoftApiConnection
-attr_accessor :api_url, :api_key
+attr_accessor :api_url, :api_key, :retry_count
 
 def initialize(url, key)
   @api_url = url
   @api_key = key
+  @retry_count = 1
 end
-
+	
 def api_perform(class_type, method, *args)
   begin
     server = XMLRPC::Client.new3({'host' => @api_url, 'path' => "/api/xmlrpc", 'port' => 443, 'use_ssl' => true})
     result = server.call("#{class_type}.#{method}", @api_key, *args)
+    if result.nil?; raise RetryException.new(true, @retry_count), "transient read error" end
+  rescue RetryException => detail
+    puts "*** INFUSION API ERROR: Transient read error *** retrying: #{@retry_count}"
+    self.retry_count += 1
+    retry if detail.ok_to_retry
   rescue XMLRPC::FaultException => e
-    puts "*** Infusion Error: #{e.faultCode} - #{e.faultString} ***"
+    puts "*** INFUSION API ERROR: #{e.faultCode} - #{e.faultString} ***"
   end
 
-  retry_call(class_type, method, *args) unless result # Infusionsoft API drops
   return result
 end
 
-def retry_call(class_type, method, *args)
-  api_perform(class_type, method, *args)
 end
 
+class RetryException < RuntimeError
+  attr :ok_to_retry
+  def initialize(ok_to_retry, retry_count)
+    @ok_to_retry = retry_count < 5 ? ok_to_retry : false
+  end
 end
 
 module ApiInfusionsoft
-  
+
   # Set account specific InfusionsoftApiConnection obj
   def self.set_account_apiconn(url, key)
     if ac = Thread.current[:api_conn]
