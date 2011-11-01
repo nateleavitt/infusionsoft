fb20d66a29af3411c56325bab17502e6a4f89e65
Nathan Leavitt <nate@infusedsystems.com>
2011-11-01 10:59:23 -0700

    updated gem version 1.0.5

--- lib/infusionsoft/version.rb
+++ lib/infusionsoft/version.rb
@@ -1,4 +1,4 @@
 module Infusionsoft
   # The version of the gem
-  VERSION = '1.0.4'.freeze unless defined?(::Infusionsoft::VERSION)
+  VERSION = '1.0.5'.freeze unless defined?(::Infusionsoft::VERSION)
 end
