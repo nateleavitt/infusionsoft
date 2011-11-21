abfd1870980e54aa8ea159f53760509a39309cdd
Nathan Leavitt <nate@infusedsystems.com>
2011-11-21 14:57:35 -0700

    added some basic error handling

--- lib/infusionsoft/connection.rb
+++ lib/infusionsoft/connection.rb
@@ -4,45 +4,32 @@ module Infusionsoft
   module Connection
     private
 
+    @retry_count = 0
+
     def connection(service_call, *args)
       server = XMLRPC::Client.new3({
-        'host' => api_url, 
-        'path' => "/api/xmlrpc", 
-        'port' => 443, 
+        'host' => api_url,
+        'path' => "/api/xmlrpc",
+        'port' => 443,
         'use_ssl' => true
       })
-      begin 
-      result = server.call("#{service_call}", api_key, *args)
+      begin
+        result = server.call("#{service_call}", api_key, *args)
+        if result.nil?; result = [] end
+      rescue Timeout::Error
+        @retry_count += 1
+        puts "*** INFUSION API TIMEOUT: retrying #{@retry_count} ***"
+        retry if ok_to_retry
       rescue XMLRPC::FaultException => e
-          puts "*** INFUSION API ERROR: #{e.faultCode} - #{e.faultString} ***"
+        puts "*** INFUSION API ERROR: #{e.faultCode} - #{e.faultString} ***"
       end
+
       return result
     end
 
-    #end
-        #if result.nil?; raise RetryException.new(true, @retry_count), "transient read error" end
-      #rescue RetryException => detail
-        ## Catch for transietn Infusionsoft connection errors
-        #puts "*** INFUSION API ERROR: Transient read error *** retrying: #{@retry_count}"
-        #self.retry_count += 1
-        #retry if detail.ok_to_retry
-      #rescue XMLRPC::FaultException => e
-        #puts "*** INFUSION API ERROR: #{e.faultCode} - #{e.faultString} ***"
-      #rescue Timeout::Error
-        #detail = RetryException.new(true, @retry_count)
-        #puts "*** INFUSION API ERROR: Timeout *** retrying #{@retry_count}"
-        #@retry_count += 1
-        #retry if detail.ok_to_retry
-      #rescue
-        #detail = RetryException.new(true, @retry_count)
-        ## This is most likely a catch for the infamous Content-Type bug from Infusionsoft.
-        ## Their response has an issue where it isn't sending back a Content-Type in the header,
-        ## which throws an nill error in the xml-rpc client in Ruby.
-        ## see xmlrcp/client.rb:552 and xmlrpc/utils.rb:159 in Ruby Standard Library.
-        #puts "*** INFUSION API ERROR: Content Type is nil back from Infusionsoft *** retrying: #{@retry_count}"
-        #@retry_count += 1
-        #retry if detail.ok_to_retry
-      #end
+    def ok_to_retry
+      @retry_count <= 5 ? true : false
+    end
 
   end
 end
