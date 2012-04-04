cb1ecc51dce5ba3c818af0dfc43b8f4703f9abe8
Nathan Leavitt <nate@infusedsystems.com>
2012-04-04 00:42:23 -0700

    fixed methods for some services bumped version

--- lib/infusionsoft/client/ticket.rb
+++ lib/infusionsoft/client/ticket.rb
@@ -4,13 +4,13 @@ module Infusionsoft
       # add move notes to existing tickets
       def ticket_add_move_notes(ticket_list, move_notes, 
                                 move_to_stage_id, notify_ids)
-        response = get('ServiceCallService', 'addMoveNotes', ticket_list, 
+        response = get('ServiceCallService.addMoveNotes', ticket_list, 
                        move_notes, move_to_stage_id, notify_ids)
       end
 
       # add move notes to existing tickets
       def ticket_move_stage(ticket_id, ticket_stage, move_notes, notify_ids)
-        response = get('ServiceCallService', 'moveTicketStage', 
+        response = get('ServiceCallService.moveTicketStage', 
                        ticket_id, ticket_stage, move_notes, notify_ids)
       end
     end
