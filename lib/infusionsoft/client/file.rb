fac915f4d02a39c1a670b4d9942bb9d2173bf64f
Nathan Leavitt <nate@infusedsystems.com>
2012-04-04 00:42:23 -0700

    fixed methods for some services bumped version

--- lib/infusionsoft/client/file.rb
+++ lib/infusionsoft/client/file.rb
@@ -2,24 +2,24 @@ module Infusionsoft
   class Client
     module File
       def file_upload(contact_id, file_name, encoded_file_base64)
-        response = get('FileService', 'uploadFile', contact_id, file_name, encoded_file_base64)
+        response = get('FileService.uploadFile', contact_id, file_name, encoded_file_base64)
       end
 
       # returns the Base64 encoded file contents
       def file_get(id)
-        response = get('FileService', 'getFile', id)
+        response = get('FileService.getFile', id)
       end
 
       def file_url(id)
-        response = get('FileService', 'getDownloadUrl', id)
+        response = get('FileService.getDownloadUrl', id)
       end
 
       def file_rename(id, new_name)
-        response = get('FileService', 'renameFile', id, new_name)
+        response = get('FileService.renameFile', id, new_name)
       end
 
       def file_replace(id, encoded_file_base64)
-        response = get('FileService', 'replaceFile', id, encoded_file_base64)
+        response = get('FileService.replaceFile', id, encoded_file_base64)
       end
     end
   end
