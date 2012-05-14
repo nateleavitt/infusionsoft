d47693b50b7b4fe00a5f034ab4e41a31aa32fb37
Nathan Leavitt <nate@infusedsystems.com>
2012-05-14 10:30:45 -0700

    adding new creditcard service

---
+++ lib/infusionsoft/client/credit_card.rb
@@ -0,0 +1,20 @@
+module Infusionsoft
+  class Client
+    # CreditCardSubmission service is used to obtain a token that is to be submitted 
+    # through a form when adding a credit card. In accordance with PCI compliance, 
+    # adding credit cards through the API will no longer be supported.
+    module CreditCard
+
+      # This service will request a token that will be used when submitting
+      # a new credit card through a form
+      #
+      # @param [Integer] contact_id of the Infusionsoft contact
+      # @param [String] url that will be redirected to upon successfully adding card
+      # @param [String] url that will be redirected to when there is a failure upon adding card
+      def request_token(contact_id, success_url, failure_url)
+        response = get('CreditCardSubmissionService', contact_id, success_url, failure_url)
+      end
+
+    end
+  end
+end
