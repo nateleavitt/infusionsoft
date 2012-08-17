2bc6bc7eabe32c97b9622eefa8d2a3100fa07865
Nathan Leavitt <nate@infusedsystems.com>
2012-08-17 11:53:07 -0700

    adding some more error checking and retries

--- lib/infusionsoft/version.rb
+++ lib/infusionsoft/version.rb
@@ -1,4 +1,4 @@
 module Infusionsoft
   # The version of the gem
-  VERSION = '1.1.0a'.freeze unless defined?(::Infusionsoft::VERSION)
+  VERSION = '1.1.0b'.freeze unless defined?(::Infusionsoft::VERSION)
 end
