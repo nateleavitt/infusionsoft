b78e57c0a854998238b6f53fe0e777040c5a6b73
Nathan Leavitt <nateleavitt@gmail.com>
2013-06-11 15:35:26 -0700

    adding args into logger

--- lib/infusionsoft/connection.rb
+++ lib/infusionsoft/connection.rb
@@ -12,7 +12,7 @@ module Infusionsoft
         'use_ssl' => true
       })
       begin
-        chlogger.info "INFUSION API call:#{service_call} api_key: #{api_key} at: #{Time.now}"
+        chlogger.info "INFUSION API call:#{service_call} api_key:#{api_key} at:#{Time.now} args:#{*args}"
         result = server.call("#{service_call}", api_key, *args)
         if result.nil?; ok_to_retry('nil response') end
       rescue XMLRPC::Client::InfusionAPINilContentTypeError => nil_content
