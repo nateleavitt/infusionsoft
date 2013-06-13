83145af5c428b7e5e51d5f553910f5fee33acdce
Nathan Leavitt <nateleavitt@gmail.com>
2013-06-13 14:21:13 -0700

    adding inputy msgs for logger

--- lib/infusionsoft/api_logger.rb
+++ lib/infusionsoft/api_logger.rb
@@ -2,8 +2,14 @@ module Infusionsoft
   class APILogger
 
     def info; end
+    def info(msg); $stdout.print msg end
+
     def warn; end
+    def warn(msg); $stdout.print msg end
+
     def error; end
+    def error(msg); $stdout.print msg end
+
     # def debug; end
     # def fatal; end
   end
