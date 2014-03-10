269abc5a9bf9ffe00f27572206f3c63ab873f130
Nathan Leavitt <nateleavitt@gmail.com>
2014-03-10 16:15:25 -0700

    v1.1.7

--- lib/infusionsoft/version.rb
+++ lib/infusionsoft/version.rb
@@ -1,4 +1,4 @@
 module Infusionsoft
   # The version of the gem
-  VERSION = '1.1.7b'.freeze unless defined?(::Infusionsoft::VERSION)
+  VERSION = '1.1.7'.freeze unless defined?(::Infusionsoft::VERSION)
 end
