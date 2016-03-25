5051edb231d3d673768c616639a564aa4fa29faf
Andy Mockli <andym@wevorce.com>
2016-03-25 11:02:39 -0600

    Revert "Added AffiliateProgram resources to client."
    This reverts commit 8bca5b108abe5f001f6ab895a4de3fe49f7a41d5.

--- lib/infusionsoft/client.rb
+++ lib/infusionsoft/client.rb
@@ -11,7 +11,6 @@ module Infusionsoft
     require 'infusionsoft/client/invoice'
     require 'infusionsoft/client/data'
     require 'infusionsoft/client/affiliate'
-    require 'infusionsoft/client/affiliate_program'
     require 'infusionsoft/client/file'
     require 'infusionsoft/client/ticket' # Deprecated by Infusionsoft
     require 'infusionsoft/client/search'
@@ -23,7 +22,6 @@ module Infusionsoft
     include Infusionsoft::Client::Invoice
     include Infusionsoft::Client::Data
     include Infusionsoft::Client::Affiliate
-    include Infusionsoft::Client::AffiliateProgram
     include Infusionsoft::Client::File
     include Infusionsoft::Client::Ticket # Deprecated by Infusionsoft
     include Infusionsoft::Client::Search
