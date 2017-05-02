6406f41296af8896238a4eff6a55e76d4ce03c71
Jared Jackson <jared@christiancourier.com>
2017-05-02 00:31:47 -0500

    adding replacement method for invoice_add_recurring_order

--- test/client/invoice_test.rb
+++ test/client/invoice_test.rb
@@ -2,10 +2,17 @@ require_relative('../test_helper')
 
 class InvoiceTest < Test::Unit::TestCase
 
-
   def test_invoice_add_recurring_order
     VCR.use_cassette('invoice_add_recurring_order') do
-      result = Infusionsoft.invoice_add_recurring_order(1, true, 1, 1, 10.0, true, 1, 1, 0, 0)
+      result = Infusionsoft.invoice_add_recurring_order(4095, true, 19, 4, 9957, 0, 0)
+
+      assert_instance_of Fixnum, result
+    end
+  end
+
+  def test_invoice_add_subscription
+    VCR.use_cassette('invoice_add_subscription') do
+      result = Infusionsoft.invoice_add_subscription(4095, true, 19, 1, 27.0, false, 4, 9957, 0, 0)
 
       assert_instance_of Fixnum, result
     end
