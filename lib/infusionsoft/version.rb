700a84e9a3856200379f8ccf28d27ff24ec4a7cd
Nathan Leavitt <nateleavitt@gmail.com>
2019-01-11 10:47:22 -0700

    Bumping to 1.3.1a

--- lib/infusionsoft/version.rb
+++ lib/infusionsoft/version.rb
@@ -1,4 +1,8 @@
 module Infusionsoft
   # The version of the gem
+<<<<<<< HEAD
   VERSION = '1.3.0a'.freeze unless defined?(::Infusionsoft::VERSION)
+=======
+  VERSION = '1.3.1a'.freeze unless defined?(::Infusionsoft::VERSION)
+>>>>>>> Bumping to 1.3.1a
 end
