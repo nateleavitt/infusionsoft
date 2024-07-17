908bad9ab2e90499a706e74a440fc86d57307fb5
Nathan Leavitt <nateleavitt@gmail.com>
2024-07-17 12:11:52 -0700

    Adding v2 option for rest url

--- lib/infusionsoft/request.rb
+++ lib/infusionsoft/request.rb
@@ -8,33 +8,33 @@ module Infusionsoft
     end
 
     # Perform an GET request
-    def get(path, token, query: {})
-      request(:get, path, token, query: query )
+    def get(path, token, query: {}, version: 'v1')
+      request(:get, path, token, query: query, version)
     end
 
-    def post(path, token, query: {}, payload: {})
-      request(:post, path, token, query, payload)
+    def post(path, token, query: {}, payload: {}, version: 'v1')
+      request(:post, path, token, query, payload, version)
     end
 
     # Perform an HTTP PUT request
-    def put(path, token, query: {}, payload: {})
-      request(:put, path, token, query, payload)
+    def put(path, token, query: {}, payload: {}, version: 'v1')
+      request(:put, path, token, query, payload, version)
     end
 
     # Perform an HTTP PATCH request
-    def patch(path, token, query: {}, payload: {})
-      request(:patch, path, token, query, payload)
+    def patch(path, token, query: {}, payload: {}, version: 'v1')
+      request(:patch, path, token, query, payload, version)
     end
 
     # Perform an HTTP DELETE request
-    def delete(path, token, query: {})
-      request(:delete, path, token, query)
+    def delete(path, token, query: {}, version: 'v1')
+      request(:delete, path, token, query, version)
     end
 
     private
 
     # Perform request
-    def request(method, path, token, query={}, payload={})
+    def request(method, path, token, query={}, payload={}, version)
       path = "/#{path}" unless path.start_with?('/')
       header = {
         authorization: "Bearer #{token.access_token}",
@@ -47,6 +47,11 @@ module Infusionsoft
         url: "https://api-intg.infusiontest.com/crm/rest/v1" + path,
         headers: header
       }
+      # Use v2 if specified
+      if version == 'v2'
+        opts[:url] = "https://api-intg.infusiontest.com/crm/rest/v2" + path
+      end
+
       opts.merge!( { payload: payload.to_json }) unless payload.empty?
       resp = RestClient::Request.execute(opts)
       return JSON.parse(resp.body) if resp.body # Some calls respond w nothing
