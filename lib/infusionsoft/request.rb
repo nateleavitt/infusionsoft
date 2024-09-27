c853f622999364ae1a8b4e26dd7d0931fac21940
Nathan Leavitt <nateleavitt@gmail.com>
2024-09-26 19:35:56 -0700

    Adding payload to delete call

--- lib/infusionsoft/request.rb
+++ lib/infusionsoft/request.rb
@@ -9,32 +9,32 @@ module Infusionsoft
 
     # Perform an GET request
     def get(path, token, query: {}, version: 'v1')
-      request(:get, path, token, query: query, version)
+      request(:get, path, token, version, query: query)
     end
 
     def post(path, token, query: {}, payload: {}, version: 'v1')
-      request(:post, path, token, query, payload, version)
+      request(:post, path, token, version, query, payload)
     end
 
     # Perform an HTTP PUT request
     def put(path, token, query: {}, payload: {}, version: 'v1')
-      request(:put, path, token, query, payload, version)
+      request(:put, path, token, version, query, payload)
     end
 
     # Perform an HTTP PATCH request
     def patch(path, token, query: {}, payload: {}, version: 'v1')
-      request(:patch, path, token, query, payload, version)
+      request(:patch, path, token, version, query, payload)
     end
 
     # Perform an HTTP DELETE request
-    def delete(path, token, query: {}, version: 'v1')
-      request(:delete, path, token, query, version)
+    def delete(path, token, query: {}, payload: {}, version: 'v1')
+      request(:delete, path, token, version, query, payload)
     end
 
     private
 
     # Perform request
-    def request(method, path, token, query={}, payload={}, version)
+    def request(method, path, token, version, query={}, payload={})
       path = "/#{path}" unless path.start_with?('/')
       header = {
         authorization: "Bearer #{token.access_token}",
