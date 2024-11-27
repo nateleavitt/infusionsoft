5040cef187a08525d7f23156ce2e6cade5f10903
Nathan Leavitt <nateleavitt@gmail.com>
2024-11-27 13:38:04 -0700

    bumping to version 1.4.0

--- lib/infusionsoft/version.rb
+++ lib/infusionsoft/version.rb
@@ -1,4 +1,4 @@
 module Infusionsoft
   # The version of the gem
-  VERSION = '1.3.9'.freeze unless defined?(::Infusionsoft::VERSION)
+  VERSION = '1.4.0'.freeze unless defined?(::Infusionsoft::VERSION)
 end
